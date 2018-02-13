bd84842653f90afaf913f85512c999761d086ae5
Elizabeth Wong <33499858+lizzwong@users.noreply.github.com>
2018-02-13 13:17:29 -0600

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,52 @@
+# prime-solo-sql
+
+1)
+          1	shawn	chicago	5	10	355.8
+          5	anthony	chicago	0	0	0
+          7	davey	chicago	9	99	98.04
+
+
+2)
+          1	shawn	chicago	5	10	355.8
+          3	larry	minneapolis	3	4	77.01
+          4	dallas	new york	6	12	0.99
+          5	anthony	chicago	0	0	0
+          6	travis	miami	1	100	500000.34
+          7	davey	chicago	9	99	98.04
+          8	ora	phoenix	88	90	3.33
+          9	grace	miami	7	9100	34.78
+
+3)
+          5	anthony	chicago	0	0	100
+
+
+4) 
+          1	shawn	chicago	5	10	355.8
+          2	cherise	minneapolis	9	9	4000
+          4	dallas	new york	6	12	0.99
+          6	travis	miami	1	100	500000.34
+          7	davey	chicago	9	99	98.04
+          8	ora	phoenix	88	90	3.33
+          9	grace	miami	7	9100	34.78
+          10	hope	phoenix	4	10	50.17
+5)
+          6	travis	miami	1	100	500000.34
+          2	cherise	minneapolis	9	9	4000
+          1	shawn	chicago	5	10	355.8
+
+6)
+          5	anthony	chicago	0	0	0
+          4	dallas	new york	6	12	0.99
+          8	ora	phoenix	88	90	3.33
+          
+7)       1	shawn	chicago	5	10	355.8
+         2	cherise	minneapolis	9	9	4000
+         6	travis	miami	1	100	500000.34   
+         
+8)       
+         11	elton	boston	14	25	13205
+         
+9)
+          6	travis	miami	1	100	500000.34
+          9	grace	miami	7	9100	34.78
+          10	hope	phoenix	4	10	50.17
